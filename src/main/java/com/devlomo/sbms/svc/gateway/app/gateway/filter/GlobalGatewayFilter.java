907b4cbddb911480b4c0312a75aa04547b8ac4ac
Jonathan Guzman Guadarrama <jonathancggs@gmail.com>
2022-04-20 10:57:49 -0500

    :sparkles: Pre & Post filters
    adding the needed code for the pre and postfilter, also make the filter orderded by given the number 1

--- src/main/java/com/devlomo/sbms/svc/gateway/app/gateway/filter/GlobalGatewayFilter.java
+++ src/main/java/com/devlomo/sbms/svc/gateway/app/gateway/filter/GlobalGatewayFilter.java
@@ -1,7 +1,10 @@
 package com.devlomo.sbms.svc.gateway.app.gateway.filter;
 
+import java.util.Optional;
+
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
@@ -12,21 +15,40 @@ import reactor.core.publisher.Mono;
 
 @Slf4j
 @Component
-public class GlobalGatewayFilter implements GlobalFilter{
+public class GlobalGatewayFilter implements GlobalFilter, Ordered {
 
 	@Override
 	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
 		log.warn("excecuting the prefilter");
+		
+		exchange.getRequest().mutate().headers(h -> h.add("token", "123456"));
+		
+		
+		// before the return all process is on the prefilter
 		return chain.filter(exchange).then(Mono.fromRunnable(() -> {
 			log.warn("excecuting the postfilter");
+			
+			Optional.ofNullable(exchange.getRequest()
+										.getHeaders()
+										.getFirst("token"))
+					.ifPresent(v -> {
+				exchange.getResponse().getHeaders().add("token", "123456");
+			});
+			
 			exchange.getResponse()
 					.getCookies()
 					.add("env", ResponseCookie.from("env", "dev")
 							.build());
-			exchange.getResponse()
+			/*exchange.getResponse()
 					.getHeaders()
-					.setContentType(MediaType.TEXT_PLAIN);	
+					.setContentType(MediaType.TEXT_PLAIN);*/	
 		}));
+	}
+
+	@Override
+	public int getOrder() {
+		// TODO Auto-generated method stub
+		return 1;
 	}
 
 }
